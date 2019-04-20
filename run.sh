8c0f5aa65c7f70a6cb677bec6b613262b1042deb
praveen <praveenstr60@gmail.com>
2019-04-20 11:14:01 +0000

    Clair scanner docker image added

--- run.sh
+++ run.sh
@@ -5,3 +5,14 @@ sudo docker-compose stop
 echo "### Initialiazing magento2 build...."
 sudo  docker-compose up -d
 echo  "##Docker building and launching successfully completed...."
+
+
+echo "### ........"
+echo "### Building the Clair Scanner"
+cd /clair_utility/
+sudo docker-compose up -d
+
+
+echo "### Clair Scanner building successful"
+cd ../
+
